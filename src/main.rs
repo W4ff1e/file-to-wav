821796e43dec71500d2a97aaf5bc807caf5503e3
W4ff1e <72278085+W4ff1e@users.noreply.github.com>
2024-07-27 12:26:44 -0400

    Added encode functionality

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
 use std::env;
-use std::f32::consts::PI;
-use std::fs::read_to_string;
-use std::i16;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::io::Error;
 use std::path::Path;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let target_file = &args[1];
     let mut output_file = &args[2];
+    let operation = &args[3];
 
+    if operation == "encode" {
+        encode(target_file, output_file);
+    } else if operation == "decode" {
+        decode(target_file, output_file);
+    } else {
+        panic!("Invalid operation");
+    }
+}
+
+fn file_exists(target_file: &Path) -> Result<bool, Error> {
+    let target_file_path = std::path::Path::new(target_file);
+    if target_file_path.exists() {
+        Ok(true)
+    } else {
+        Err(Error::new(std::io::ErrorKind::NotFound, "File not found"))
+    }
+}
+
+fn encode(target_file: &str, output_file: &str) {
+    println!("Encoding file");
+    println!("Target file: {}, Output file: {}", target_file, output_file);
     let target_file_path = std::path::Path::new(target_file);
     // let output_file_path = std::path::Path::new(output_file);
 
@@ -20,7 +42,7 @@ fn main() {
 
     println!("Target file: {}, Output file: {}", target_file, output_file);
 
-    let target_file_path = match file_exists(target_file_path) {
+    let _target_file_path = match file_exists(target_file_path) {
         Ok(true) => true,
         Ok(false) => panic!("File not found"),
         Err(_) => panic!("File not found"),
@@ -33,19 +55,29 @@ fn main() {
         sample_format: hound::SampleFormat::Int,
     };
     let mut writer = hound::WavWriter::create(output_file, spec).unwrap();
-    for t in (0..44100).map(|x| x as f32 / 44100.0) {
-        let sample = (t * 440.0 * 2.0 * PI).sin();
+
+    let mut target_file = File::open(target_file).unwrap();
+    let mut data = vec![];
+    target_file.read_to_end(&mut data).unwrap();
+
+    // println!("Data: {:?}", data);
+
+    for byte in data {
+        let sample = byte as f32 / 255.0;
         let amplitude = i16::MAX as f32;
         writer.write_sample((sample * amplitude) as i16).unwrap();
     }
     writer.finalize().unwrap();
 }
 
-fn file_exists(target_file: &Path) -> Result<bool, Error> {
-    let target_file_path = std::path::Path::new(target_file);
-    if target_file_path.exists() {
-        Ok(true)
-    } else {
-        Err(Error::new(std::io::ErrorKind::NotFound, "File not found"))
-    }
+fn decode(target_file: &str, output_file: &str) {
+    todo!()
+    // println!("Decoding file");
+    // println!("Target file: {}, Output file: {}", target_file, output_file);
+
+    // let mut reader = hound::WavReader::open(target_file).unwrap();
+
+    // let data: i16 = reader.samples::<i16>().collect().unwrap();
+    // let mut output_file = File::create(output_file).unwrap();
+    // output_file.write_all(&data.collect::<Vec<u8>>()).unwrap();
 }
